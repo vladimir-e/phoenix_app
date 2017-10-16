b5794299a450a80cacebde670151323bcb0296fc
Vladimir E <evdokimov.vladimir@gmail.com>
2017-10-16 10:51:59 +0300

    Remove prod.secrets.exs

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :depltest, DepltestWeb.Endpoint,
   load_from_system_env: true,
+  http: [port: 8888],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
