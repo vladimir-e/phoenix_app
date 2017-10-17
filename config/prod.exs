c801cde4689d2f323f1a94aa370d8b71b89c58ef
Vladimir E <evdokimov.vladimir@gmail.com>
2017-10-17 15:09:12 +0300

    edited prod.exs to add HTTPS scheme

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :depltest, DepltestWeb.Endpoint,
   load_from_system_env: true,
   http: [port: 8888],
-  url: [host: "example.com", port: 80],
+  url: [host: "example.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
